d0cec0b24a2f906b586238138d7d2c16f0d00935
Victor <vgomezbalaguer@gmail.com>
2024-11-14 18:10:23 +0100

    Añadimos últimos controladores

--- controladores/listaProductos.php
+++ controladores/listaProductos.php
@@ -5,7 +5,7 @@ include '../vistas/listaProductos/body.html';
 include '../vistas/footer.html';
 ?>
 <script src="../vistas/js/styleListaProductos.js"></script>
-<script src="../vistas/js/verDetalle.js"></script>
+<script src="../vistas/js/listaProductos.js"></script>
 <?php
 include '../vistas/final.html';
 ?>
