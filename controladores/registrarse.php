aebf157b5b97b49f9c4980b3c11426ad8838226c
Victor <vgomezbalaguer@gmail.com>
2024-11-17 23:39:43 +0100

    Cambiamos de temática la página de registro

--- controladores/registrarse.php
+++ controladores/registrarse.php
@@ -1,8 +1,6 @@
 <?php
 include '../vistas/inicio.html';
-include '../vistas/nav.php';
 include '../vistas/registrarse/body.html';
-include '../vistas/footer.html';
 ?>
 <script src="../vistas/js/styleRegistrarse.js"></script>
 <script src="../vistas/js/registrarse.js"></script>
